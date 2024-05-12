2747631ad6c996ac658360233fcfd899ef6886d1
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-12 02:23:56 -0300

    Update script.js
    exFinal com validações de entrada

--- exFinal/script.js
+++ exFinal/script.js
@@ -7,17 +7,37 @@ let txtmaior = document.getElementById("txtmaior")
 let txtmenor = document.getElementById("txtmenor")
 let txtsoma = document.getElementById("txtsoma")
 let txtmedia = document.getElementById("txtmedia")
+
 let soma = 0
 let menor = 101
 let maior = -1
 
+window.onload = function() {
+    document.getElementById("num").focus();
+};
+
+
 function adicionar(){
+    if(num.value == ""){
+        window.alert("Insira um valor antes de adicionar");
+        num.value = ""
+        num.focus()
+    }else if(Number(num.value) < 1 || Number(num.value) > 100){
+        window.alert("Valores entre 1 - 100");
+        num.value = ""
+        num.focus()
+    }else if(vetNum.includes(Number(num.value))){
+        window.alert("Valor ja inserido");
+        num.value = ""
+        num.focus()
+    }else{
     vetNum.push(Number(num.value))
     var item = document.createElement("option");
     item.text = `${Number(num.value)}`
     caixa.appendChild(item)
     num.value = ""
     num.focus()
+    }
 }
 
 function finalizar(){
